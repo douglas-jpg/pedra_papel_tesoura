731632e48e75fce8597cf13078079e5b2d1c8407
Douglas Lima <57305990+douglas-jpg@users.noreply.github.com>
2023-04-28 21:17:56 -0300

    Finalizado
    Projeto pedra papel tesoura finalizado com todas as funções completas.

--- emo/__init__.py
+++ emo/__init__.py
@@ -1,10 +1,10 @@
 import emoji
+from time import sleep
 
 
 def pedra():
     """
     Cria o emoji de pedra
-    :return: emoji de pedra U+1FAA8
     """
     return emoji.emojize(':rock:')
 
@@ -12,7 +12,6 @@ def pedra():
 def tesoura():
     """
     Cria o emoji de tesoura
-    :return: emoji de tesoura U+FE0F
     """
     return emoji.emojize(':scissors:')
 
@@ -20,18 +19,49 @@ def tesoura():
 def papel():
     """
     Cria o emoji de papel
-    :return: emojis de papel U+1F4DC
     """
     return emoji.emojize(':scroll:')
 
 
 def vida(n=3):
     """
-    Cria a quantidade de vidas para o jogo
-    inicialmente 3 vidas
-    :param n: quantidade de corações
+    Cria a quantidade de vidas para o jogo inicialmente 3 vidas.
+    :param n: Quantidade de corações
     :return: emoji de coração U+FE0F
     """
-    for c in range(n):
-        print(emoji.emojize(':red_heart:'), end='')
+    print(n*f"{emoji.emojize(':red_heart:')}")
     return ''
+
+
+def sair():
+    """
+    Cria o emoji de xis vermelho
+    """
+    return emoji.emojize(':cross_mark:')
+
+
+def empate_pedra():
+    print(pedra(), end='')
+    sleep(1)
+    print(' x ', end='')
+    sleep(1)
+    print(pedra())
+    return ''
+
+
+def empate_papel():
+    print(papel(), end='')
+    sleep(1)
+    print(' x ', end='')
+    sleep(1)
+    print(papel())
+    return ''
+
+def empate_tesoura():
+    print(tesoura(), end='')
+    sleep(1)
+    print(' x ', end='')
+    sleep(1)
+    print(tesoura())
+    return ''
+
--- jogo/main.py
+++ jogo/main.py
@@ -1,2 +1,166 @@
+from time import sleep
+from random import randint
 
-jogo()
+import emo  # Modulo criado para facilitar os emojis.
+
+
+def jogo():
+    """
+    Função para jogar pedra papel tesoura
+    E apos terminar adiciona o nome e a
+    Pontuação em um arquivo txt.
+    """
+    vidas = 3
+    pontos = 0
+    nome = str(input('Digite o seu nome jogador: '))
+
+    sleep(0.75)
+    # Inicio do jogo até as vidas acabarem.
+    while vidas > 0:
+        print(f'\n {nome} voce tem {vidas} vidas')
+        emo.vida(vidas)
+        print(f'[1] Pedra{emo.pedra()}\n'
+              f'[2] Papel{emo.papel()}\n'
+              f'[3] Tesoura{emo.tesoura()}\n'
+              f'[999] Sair{emo.sair()}')
+        jogador = int(input('Escolha sua opção: '))
+        sleep(0.5)
+        # Tratamento de erro para caso o usuario digite errado.
+        while jogador not in [1, 2, 3, 999]:
+            print('Opção errada, tente escolher uma dessas opções: ')
+            jogador = int(input('Sua escolha: '))
+        pc = randint(1, 3)  # Escolha do computador.
+        # Caso houver empate entre o computador e o jogador.
+        if jogador == pc:
+            if jogador == 1:
+                sleep(0.5)
+                print('Ambus escolheram pedra')
+                sleep(0.5)
+                print(emo.empate_pedra())
+            elif jogador == 2:
+                sleep(0.5)
+                print('Ambus escolheram papel')
+                sleep(0.5)
+                print(emo.empate_papel())
+            else:
+                sleep(0.5)
+                print('Ambus escolheram tesoura')
+                sleep(0.5)
+                print(emo.empate_tesoura())
+            print('Empate!')
+            sleep(0.5)
+        # Caso o computador ganhe uma rodada.
+        elif (jogador == 1 and pc == 2 or
+              jogador == 2 and pc == 3 or
+              jogador == 3 and pc == 1):
+            sleep(1)
+            vidas -= 1  # O jogador perde uma vida.
+            if jogador == 1:
+                print('O jogador escolheu pedra')
+                print(f'{emo.pedra()}', end='')
+                print(' x ', end='')
+            elif jogador == 2:
+                print('O jogador escolheu papel')
+                print(f'{emo.papel()}', end='')
+                print(' x ', end='')
+            elif jogador == 3:
+                print('O jogador escolheu tesoura')
+                print(f'{emo.tesoura()}', end='')
+                print(' x ', end='')
+            if pc == 1:
+                print(emo.pedra())
+                print('O pc escolheu pedra')
+            elif pc == 2:
+                print(emo.papel())
+                print('O pc escolheu papel')
+            else:
+                print(emo.tesoura())
+                print('O pc escolheu tesoura')
+            print('Voce perdeu uma vida!')
+        # Caso o jogador ganhe uma rodada.
+        elif (jogador == 1 and pc == 3 or
+              jogador == 2 and pc == 1 or
+              jogador == 3 and pc == 2):
+            sleep(0.5)
+            pontos += 1  # É adicionado 1 ponto para o jogador.
+            if jogador == 1:
+                print('O jogador escolheu pedra')
+                print(f'{emo.pedra()}', end='')
+                print(' x ', end='')
+            elif jogador == 2:
+                print('O jogador escolheu papel')
+                print(f'{emo.papel()}', end='')
+                print(' x ', end='')
+            elif jogador == 3:
+                print('O jogador escolheu tesoura')
+                print(f'{emo.tesoura()}', end='')
+                print(' x ', end='')
+            if pc == 1:
+                print(emo.pedra())
+                print('O pc escolheu pedra')
+            elif pc == 2:
+                print(emo.papel())
+                print('O pc escolheu papel')
+            else:
+                print(emo.tesoura())
+                print('O pc escolheu tesoura')
+            print('Voce ganhou!')
+        # Caso o jogador queira abadonar a partida.
+        elif jogador == 999:
+            break
+    print('QUE PENA TODAS AS SUAS VIDAS ACAVARAM!!!')
+    # Nome e pontos do jogador são adicionados no arquivo de pontuações.
+    tabela = open('docs/pontuacao.txt', 'a', encoding='utf-8')
+    tabela.write(f"{nome:<10} {pontos:>10}\n")
+
+
+def main():
+    """
+    Função principal com o menu com várias funções.
+    """
+    print(f'{"-"*50}')  # letreiro inicial.
+    print(f'BEM VINDO AO JOGO PEDRA{emo.pedra()}'
+          f' PAPEL{emo.papel()} TESOURA{emo.tesoura()}!!!')
+    print(f'{"-" * 50}')
+    while True:
+        print('[1] Jogar\n'
+              '[2] Como Jogar\n'
+              '[3] Pontuação\n'
+              '[4] Creditos\n'
+              '[5] Sair')
+        opc = int(input(f'Escolha a opção: '))
+        #  Tratamento de erro caso o usuario digite errado.
+        while opc not in [1, 2, 3, 4, 5]:
+            opc = int(input(f'Escolha uma opção valida: '))
+        # Tela de carregamento simples.
+        print('Carregando', end='')
+        for i in range(3):
+            sleep(0.33)
+            print('.', end='')
+        print('\n')
+        # Inicia o jogo.
+        if opc == 1:
+            jogo()
+        # Abri o arquivo e imprime as regras do jogo.
+        elif opc == 2:
+            regras = open('docs/regras.txt', 'r', encoding='utf-8')
+            print(regras.read())
+            regras.close()
+        # Abre e imprime o arquivo da pontuação.
+        elif opc == 3:
+            pontuacao = open('docs/pontuacao.txt', 'r', encoding='utf-8')
+            print(pontuacao.read())
+            pontuacao.close()
+        # Abre e imprime o arquivo do criador com minhas redes sociais.
+        elif opc == 4:
+            credito = open('docs/creditos.txt', 'r', encoding='utf-8')
+            print(credito.read())
+            credito.close()
+        # Fecha o programa.
+        elif opc == 5:
+            break
+    print(f'Que pena que voce ja vai'
+          f',bem ate mais!!')
+
+
+main()
